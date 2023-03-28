454a0344c53171f81367b8fba7adb31ccdb59033
MozartOnWeb <mozartontheweb@gmail.com>
2023-03-28 13:04:48 +0000

    remove useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,23 @@ function App() {
   const [phone, setPhone] = useState<string>("");
   const [email, setEmail] = useState<string>("");
 
-  useEffect(() => {
-    window.addEventListener("message", (e) => {
-      if (e.origin !== "https://local-storage-sender.vercel.app") {
-        return;
-      }
+  window.addEventListener("message", (e) => {
+    if (e.origin !== "https://local-storage-sender.vercel.app") {
+      return;
+    }
 
-      const data = JSON.parse(e.data);
+    const data = JSON.parse(e.data);
 
-      if (typeof data.phone !== "undefined") {
-        localStorage.setItem("phone", data.phone);
-        setPhone(data.phone);
-      }
+    if (typeof data.phone !== "undefined") {
+      localStorage.setItem("phone", data.phone);
+      setPhone(data.phone);
+    }
 
-      if (typeof data.email !== "undefined") {
-        localStorage.setItem("email", data.email);
-        setEmail(data.email);
-      }
-    });
-  }, []);
+    if (typeof data.email !== "undefined") {
+      localStorage.setItem("email", data.email);
+      setEmail(data.email);
+    }
+  });
 
   return (
     <div className="App">
