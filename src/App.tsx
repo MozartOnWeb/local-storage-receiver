08829657482dc9e314c8b9740b0987bea025cf1a
MozartOnWeb <mozartontheweb@gmail.com>
2023-03-28 15:29:49 +0000

    last version

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,35 @@ function App() {
   const [phone, setPhone] = useState<string>("");
   const [email, setEmail] = useState<string>("");
 
-  window.addEventListener("message", (e) => {
-    if (e.origin !== "https://local-storage-sender.vercel.app") {
-      return;
-    }
+  useEffect(() => {
+    window.addEventListener("message", (e) => {
+      if (e.origin !== "https://local-storage-sender.vercel.app") {
+        return;
+      }
 
-    const data = JSON.parse(e.data);
+      const data = JSON.parse(e.data);
 
-    if (typeof data.phone !== "undefined") {
-      localStorage.setItem("phone", data.phone);
-      setPhone(data.phone);
-    }
+      if (typeof data.phone !== "undefined") {
+        localStorage.setItem("phone", data.phone);
+      }
 
-    if (typeof data.email !== "undefined") {
-      localStorage.setItem("email", data.email);
-      setEmail(data.email);
-    }
-  });
+      if (typeof data.email !== "undefined") {
+        localStorage.setItem("email", data.email);
+      }
+    });
+
+    const localPhone = localStorage.getItem("phone") as string;
+    const localEmail = localStorage.getItem("email") as string;
+
+    setPhone(localPhone);
+    setEmail(localEmail);
+  }, [localStorage]);
 
   return (
     <div className="App">
       <h1>Receiver</h1>
-      <p>{phone}</p>
-      <p>{email}</p>
+      <p>phone: {phone}</p>
+      <p>email: {email}</p>
     </div>
   );
 }
